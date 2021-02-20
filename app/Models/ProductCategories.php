857bb5f7e3e35f6744a4b92b0b42c4f4488fb7d7
abraham ghaemi <ig.ghaemi@gmail.com>
2021-02-21 03:10:22 +0330

    init project with laravel and set new model for product categories

---
+++ app/Models/ProductCategories.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductCategories extends Model
+{
+    use HasFactory;
+
+    protected $table = 'product_category';
+
+    protected $guarded = [];
+
+    protected $casts = [
+    	'id' => 'string'
+    ];
+}
