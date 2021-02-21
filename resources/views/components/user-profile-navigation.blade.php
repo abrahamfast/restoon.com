81ee12461017bba66a61205fac30a60a0f7445f8
abraham ghaemi <ig.ghaemi@gmail.com>
2021-02-21 05:49:40 +0330

    new components

---
+++ resources/views/components/user-profile-navigation.blade.php
@@ -0,0 +1,25 @@
+<li class="ui dropdown">
+                        <a href="#" class="opts_account">
+                            <img src="images/avatar/img-5.jpg" alt="">
+                            <span class="user__name">John Doe</span>
+                            <i class="uil uil-angle-down"></i>
+                        </a>
+                        <div class="menu dropdown_account">
+                            <div class="night_mode_switch__btn">
+                                <a href="#" id="night-mode" class="btn-night-mode">
+                                    <i class="uil uil-moon"></i> Night mode
+                                    <span class="btn-night-mode-switch">
+<span class="uk-switch-button"></span>
+                                    </span>
+                                </a>
+                            </div>
+                            <a href="dashboard_overview.html" class="item channel_item"><i class="uil uil-apps icon__1"></i>Dashbaord</a>
+                            <a href="dashboard_my_orders.html" class="item channel_item"><i class="uil uil-box icon__1"></i>My Orders</a>
+                            <a href="dashboard_my_wishlist.html" class="item channel_item"><i class="uil uil-heart icon__1"></i>My Wishlist</a>
+                            <a href="dashboard_my_wallet.html" class="item channel_item"><i class="uil uil-usd-circle icon__1"></i>My Wallet</a>
+                            <a href="dashboard_my_addresses.html" class="item channel_item"><i class="uil uil-location-point icon__1"></i>My Address</a>
+                            <a href="offers.html" class="item channel_item"><i class="uil uil-gift icon__1"></i>Offers</a>
+                            <a href="faq.html" class="item channel_item"><i class="uil uil-info-circle icon__1"></i>Faq</a>
+                            <a href="sign_in.html" class="item channel_item"><i class="uil uil-lock-alt icon__1"></i>Logout</a>
+                        </div>
+                    </li>
