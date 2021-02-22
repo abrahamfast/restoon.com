e92581fc600babac7bac83b67d4550425454917a
abraham ghaemi <ig.ghaemi@gmail.com>
2021-02-22 11:21:16 +0330

    passing data to component

--- app/View/Components/FilterTop.php
+++ app/View/Components/FilterTop.php
@@ -6,14 +6,15 @@ use Illuminate\View\Component;
 
 class FilterTop extends Component
 {
+    public $slug;
     /**
      * Create a new component instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($slug)
     {
-        //
+        $this->slug = $slug;
     }
 
     /**
