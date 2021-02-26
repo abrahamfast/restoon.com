e2546151dd7029e5229a39c7acb4875b3cfea8a8
abraham ghaemi <ig.ghaemi@gmail.com>
2021-02-26 10:08:37 +0330

    update id to safer user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,7 +23,7 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'id' => uniqid(),
+            'id' => uniqid() . substr(md5(rand()), 0, 4),
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
