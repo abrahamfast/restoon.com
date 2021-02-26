1478f5b0bf78d3a053b3887a80fa7a82535b4989
abraham ghaemi <ig.ghaemi@gmail.com>
2021-02-26 11:02:40 +0330

    wish list rel

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,4 +43,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'id' => 'string'
     ];
+
+    public function wishlist()
+    {
+        return $this->hasMany(Wishlist::class);
+    }
 }
