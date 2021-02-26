d1fe119b09cdd7a6ceac2159efaf1d50f1447c71
abraham ghaemi <ig.ghaemi@gmail.com>
2021-02-26 11:04:38 +0330

    make wish model

---
+++ app/Models/Wishlist.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Wishlist extends Model
+{
+    use HasFactory;
+
+    protected $casts = [
+    	'id' => 'string',
+    	'product_id' => 'string',
+    	'user_frontend_id' => 'string'
+    ];
+
+    public function product()
+    {
+    	return $this->hasOne(Product::class);
+    }
+}
