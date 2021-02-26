9595149d95ba06b58975b47c1499c9d2e341aa73
abraham ghaemi <ig.ghaemi@gmail.com>
2021-02-26 09:57:12 +0330

    app skeleton nav, overview ,....

---
+++ resources/views/app.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.main')
+
+@section('wrapper')
+    <x-breadcrumb :slug="'User Profile'"/>
+    <x-app.user />
+    <div class="">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-4">
+                    <x-app.navbar />
+                </div>
+                <div class="col-lg-9 col-md-8">
+                    <div class="dashboard-right">
+                        <x-app.overview/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
---
+++ resources/views/components/app/navbar.blade.php
@@ -0,0 +1,11 @@
+
+<div class="left-side-tabs">
+    <div class="dashboard-left-links">
+        <a href="/app/dashboard" class="user-item active"><i class="uil uil-apps"></i>Overview</a>
+        <a href="/app/orders" class="user-item"><i class="uil uil-box"></i>My Orders</a>
+        <a href="/app/rewards" class="user-item"><i class="uil uil-gift"></i>My Rewards</a>
+        <a href="/app/wishlist" class="user-item"><i class="uil uil-heart"></i>Shopping Wishlist</a>
+        <a href=/app/addresses" class="user-item"><i class="uil uil-location-point"></i>My Address</a>
+        <a href="/app/logout" class="user-item"><i class="uil uil-exit"></i>Logout</a>
+    </div>
+</div>
