208c6d227f4993e1cb9b56a5c5e413984c8c9b9d
abraham ghaemi <ig.ghaemi@gmail.com>
2021-02-27 10:14:59 +0330

    create qoute, contact model

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Wishlist::class, 'user_frontend_id');
     }
+
+    public function account()
+    {
+        return $this->hasOne(Account::class);
+    }
 }
