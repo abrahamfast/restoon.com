846e93108d4949d7a020daee8bca6c98981c8e89
abraham ghaemi <ig.ghaemi@gmail.com>
2021-03-05 19:41:50 +0330

    get related items

--- resources/views/components/cart/cart-item.blade.php
+++ resources/views/components/cart/cart-item.blade.php
@@ -1,17 +1,17 @@
 <div class="cart-item border_radius">
     <div class="cart-product-img">
-        <img src="/dataloader/{{ $product->getCover() }}" alt="">
-        @if($product->discount)
-            <div class="offer-badge">{{ $product->discount}} % تخفیف</div>
+        <img src="/dataloader/{{ $product->product()->first()->getCover() }}" alt="">
+        @if($product->product()->first()->discount)
+            <div class="offer-badge">{{ $product->product()->first()->discount}} % تخفیف</div>
         @endif
     </div>
     <div class="cart-text">
-        <h4>{{ $product->name }}</h4>
-        @if($item->product()->first()->discount)
-            <div class="cart-item-price">{{ $product->discount_price }} <span>{{ $product->discount_price }}</span></div>
+        <h4>{{ $product->product()->first()->name }}</h4>
+        @if($product->product()->first()->discount)
+            <div class="cart-item-price">{{ $product->product()->first()->discount_price }} <span>{{ $product->product()->first()->discount_price }}</span></div>
         @else
-            <div class="cart-item-price">{{ $product->discount_price}}</div>
+            <div class="cart-item-price">{{ $product->product()->first()->discount_price}}</div>
         @endif
-        <a href="/cart/unlink/{{$item->id}}" class="cart-close-btn"><i class="uil uil-multiply"></i></a>
+        <a href="/cart/unlink/{{$product->id}}" class="cart-close-btn"><i class="uil uil-multiply"></i></a>
     </div>
 </div>
