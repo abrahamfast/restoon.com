88ef1bc8e4fe231288decaa4859d35501cc19f63
abraham ghaemi <ig.ghaemi@gmail.com>
2021-03-08 13:30:48 +0330

    add offer route

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/faq', [FaqController::class, 'index']);
 Route::get('category/{slug}', [ProductController::class, 'index']);
 Route::get('product/{id}', [ProductController::class, 'show']);
 Route::get('offer/{id}', [OfferController::class, 'show']);
+Route::get('offer', [OfferController::class, 'index']);
 Route::get('newest', [ProductController::class, 'newest']);
 Route::get('special', [ProductController::class, 'special']);
 Route::get('contact', [ContactController::class, 'index']);
