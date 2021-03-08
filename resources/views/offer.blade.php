bb6909cc4ffd1c8a96d6c56c933bf46c860094a5
abraham ghaemi <ig.ghaemi@gmail.com>
2021-03-08 13:41:00 +0330

    update offer style and lang

--- resources/views/offer.blade.php
+++ resources/views/offer.blade.php
@@ -2,32 +2,32 @@
 
 @section('wrapper')
 		<x-breadcrumb :slug="$slug"/>
-		<div class="all-product-grid">	
+		<div class="all-product-grid mb-14">	
             <div class="container">
                 <div class="row">
 					<div class="col-lg-12 col-md-12">
                         <div class="default-title mt-4">
-                            <h2>{{ __('Offers Head') }}</h2>
+                            <h2>{{ __('global.Offers Head') }}</h2>
                             <img src="/images/line.svg" alt="">
                         </div>
+                    </div>
                         @if($offers)
                         	@foreach($offers as $offer)
                         	<div class="col-lg-4">
-                        <a href="#" class="offers-item">
-                            <div class="offer-img">
-                                <img src="/dataloader/{{ $offer->cover_id}}" alt="">
-                            </div>
-                            <div class="offers-text">
-                                <h4>📢 {{ $offer->name }}</h4>
-                                <p>{{ $offer->description }}</p>
-                            </div>
-                        </a>
-                    </div>
+		                        <a href="/offer/{{$offer->id}}" class="offers-item">
+		                            <div class="offer-img">
+		                                <img src="/dataloader/{{ $offer->cover_id}}" alt="">
+		                            </div>
+		                            <div class="offers-text">
+		                                <h4>📢 {{ $offer->name }}</h4>
+		                                <p>{{ $offer->description }}</p>
+		                            </div>
+		                        </a>
+		                    </div>
                         	@endforeach
                         @endif
                     </div>
                 </div>
             </div>
-		</div>
 
 @stop
