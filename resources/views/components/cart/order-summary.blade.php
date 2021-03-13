bc48c59ef2741cc506fc613ae1a1c7526ae5cd8e
abraham ghaemi <ig.ghaemi@gmail.com>
2021-03-13 05:23:28 +0330

    checkout order summary

--- resources/views/components/cart/order-summary.blade.php
+++ resources/views/components/cart/order-summary.blade.php
@@ -4,28 +4,27 @@
                             </div>
                             <div class="right-cart-dt-body">
                                 @if($quote)
-                                    @foreach($quote->items()->get() as $product)
-                                        <x-cart.cart-item :product="$product" />
+                                    @foreach($quote->items()->get() as $item)
+                                        <x-cart.cart-item :product="$item" />
                                     @endforeach
                                 @endif
                             </div>
                             <div class="total-checkout-group">
                                 <div class="cart-total-dil">
-                                    <h4>Super Market</h4>
-                                    <span>$15</span>
+                                    <h4>{{ __('global.Your cart Super Market price') }}</h4>
+                                    <span>{{ __('global.toman', ["price" => $item->takeCurrency($quote->amount) ]) }}</span>
                                 </div>
                                 <div class="cart-total-dil pt-3">
-                                    <h4>Delivery Charges</h4>
-                                    <span>$1</span>
+                                    <h4>{{ __('global.Delivery Charges') }}</h4>
+                                    <span>{{ __('global.toman', ["price" => $item->takeCurrency($quote->shipping_cost) ]) }}</span>
                                 </div>
                             </div>
                             <div class="cart-total-dil saving-total ">
-                                <h4>Total Saving</h4>
-                                <span>$3</span>
-                            </div>
+                                <h4>{{ __('global.Total Saving') }}</h4>
+                                <span>{{ __('global.toman', ["price" => $item->takeCurrency($quote->pre_discounted_amount) ]) }}</span>                            </div>
                             <div class="main-total-cart">
-                                <h2>Total</h2>
-                                <span>$16</span>
+                                <h2>{{ __('global.Total') }}</h2>
+                                <span>{{ __('global.toman', ["price" => $item->takeCurrency($quote->grand_total_amount) ]) }}</span>
                             </div>
                             <div class="payment-secure">
                                 <i class="uil uil-padlock"></i>{{ __('global.Secure checkout') }}
