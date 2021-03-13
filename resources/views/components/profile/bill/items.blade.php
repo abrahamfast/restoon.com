32dbd6a9a57b373a952f3bdd65a3acb21f00ea63
abraham ghaemi <ig.ghaemi@gmail.com>
2021-03-13 06:30:50 +0330

    item name added

--- resources/views/components/profile/bill/items.blade.php
+++ resources/views/components/profile/bill/items.blade.php
@@ -5,7 +5,7 @@
     <div class="bill-descp">
         <div class="itm-ttl">{{ __('global.bill-itemsCount', ['count' => $items->count()]) }}</div>
         @foreach($items->get() as $item)
-        	<div class="item-prdct">{{ $item->quantity }} تا {{ __('global.' . $item->product->unit_weight, ['num' => $item->product->weight]) }}</div>
+        	<div class="item-prdct"><h4>{{ $item->name }}</h4> : {{ $item->quantity }} تا {{ __('global.' . $item->product->unit_weight, ['num' => $item->product->weight]) }}</div>
         @endforeach
     </div>
 </div>
