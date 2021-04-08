86280d859f4fd21b0a6ceff895db12879bfc82bc
abraham ghaemi <ig.ghaemi@gmail.com>
2021-04-08 18:04:25 +0430

    update contact before save

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -15,13 +15,15 @@ class ContactController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => 'required|max:50|min:20',
-            'subject' => 'required|max:50|min:20',
+            'name' => 'required|max:50',
+            'subject' => 'required|max:50',
             'email' => 'required|email',
             'description' => 'required'
         ]);
 
-        Ticket::create($request->all());
+        $data = $request->except('/contact', '_token');
+        $data['id'] = uniqid() . substr(md5(rand()), 0, 4);
+        Ticket::create($data);
 
         return redirect()->route('contact')->with([
             'flush' => __('global.success your request')
