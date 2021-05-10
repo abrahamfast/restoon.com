ab96e1b239569be54023212f109ed9e67c5b9e79
abraham ghaemi <ig.ghaemi@gmail.com>
2021-05-10 12:44:38 +0430

    point disable

--- resources/views/components/app/user.blade.php
+++ resources/views/components/app/user.blade.php
@@ -12,7 +12,6 @@
                     </div>
                     <h4>{{ $user->name }}</h4>
                     <p>{{ $user->phone }}<a href="#"><i class="uil uil-edit"></i></a></p>
-                    <div class="earn-points"><img src="/images/Dollar.svg" alt="">{{ __('global.Points') }} : <span>20</span></div>
                 </div>
             </div>
         </div>
---
+++ resources/views/components/profile/earn-point.blade.php
@@ -0,0 +1,5 @@
+<div class="earn-points">
+    <img src="/images/Dollar.svg" alt="">
+    {{ __('global.Points') }} : 
+    <span>{{ $points }}</span>
+</div>
